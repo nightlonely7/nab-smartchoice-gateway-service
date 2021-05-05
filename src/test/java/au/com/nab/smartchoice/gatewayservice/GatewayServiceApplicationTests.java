223f0f14cfddaea68104758702a65038c6e43e34
nightlonely7 <Speedup79>
2021-05-06 03:27:10 +0700

    Initial commit

---
+++ src/test/java/au/com/nab/smartchoice/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nab.smartchoice.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
